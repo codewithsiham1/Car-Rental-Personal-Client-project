3aa866dfcb723c29d92f06292f5ee6ee13c7c64b
moudud <sihamislam18@gmail.com>
2025-06-23 01:27:34 +0600

    all of use known well

--- src/Page/Dashboard/Payment/Payment.jsx
+++ src/Page/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,17 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
-
+import CheckOut from '../CheckOut/CheckOut';
+// TODO:add publishibler key
+const stripePromise=loadStripe(import.meta.env. VITE_PAYMENT_GATEWAY_PK)
 const Payment = () => {
     return (
         <div>
-            
+            <div>
+                <Elements stripe={stripePromise}>
+                <CheckOut></CheckOut>
+                </Elements>
+            </div>
         </div>
     );
 };
