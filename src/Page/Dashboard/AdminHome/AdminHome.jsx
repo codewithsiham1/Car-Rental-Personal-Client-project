86c911c76571193bc6c1111b908781607a93dc11
moudud <sihamislam18@gmail.com>
2025-06-24 17:28:35 +0600

    anage all rare soluation

--- src/Page/Dashboard/AdminHome/AdminHome.jsx
+++ src/Page/Dashboard/AdminHome/AdminHome.jsx
@@ -9,8 +9,8 @@ const AdminHome = () => {
           <Helmet>
         <title>Admin Dashboard | Smart StudyHub</title>
       </Helmet>
-       <div>
-         <h1 className='text-2xl font-bold'>Hi ,Welcome <span>{user?.displayName ? user.displayName : 'Back'}</span></h1>
+       <div className='flex flex-col justify-center items-center'>
+         <h1 className='text-2xl font-bold text-blue-400'>Hi ,Welcome <span>{user?.displayName ? user.displayName : 'Back'}</span></h1>
           <p className="mt-4">Manage users, view reports, and control the system here.</p>
        </div>
        </>
